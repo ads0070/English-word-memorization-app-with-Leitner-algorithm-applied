ec22540a4fd2c418c6238f0d8e4ead41405b5428
김기태 <rlarlxo2323@naver.com>
2022-02-28 21:00:51 +0900

    upload main layout develop

--- app/src/main/java/com/example/memvoca/MainActivity.java
+++ app/src/main/java/com/example/memvoca/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.memvoca;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    Fragment homeFragment, trashFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeFragment = new HomeFragment();
+        trashFragment = new TrashFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frame, trashFragment).commit();
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+                if (position == 0) {
+                    selected = homeFragment;
+                } else if (position == 1) {
+                    selected = trashFragment;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
