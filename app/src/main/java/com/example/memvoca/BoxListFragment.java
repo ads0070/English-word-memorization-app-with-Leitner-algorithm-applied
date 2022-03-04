e5db511787ab04bc9d7ccd64a4cceb8773decea3
gitae Kim <rlarlxo2323@naver.com>
2022-03-04 17:33:13 +0900

    new layout(Box List)

---
+++ app/src/main/java/com/example/memvoca/BoxListFragment.java
@@ -0,0 +1,25 @@
+package com.example.memvoca;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BoxListFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.activity_function, container, false);
+        return inflater.inflate(R.layout.fragment_box_list, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/memvoca/FunctionActivity.java
@@ -0,0 +1,27 @@
+package com.example.memvoca;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class FunctionActivity extends AppCompatActivity {
+
+    private  BoxListFragment boxListFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function);
+
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        boxListFragment = new BoxListFragment();
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.func_frame, boxListFragment).commit();
+    }
+}
