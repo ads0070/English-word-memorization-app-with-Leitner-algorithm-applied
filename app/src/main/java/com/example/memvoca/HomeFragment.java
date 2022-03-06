5284c6e206ee3d458eb301e730e34cc0e3cf49ff
ads0070 <gch05610@gmail.com>
2022-03-06 19:36:28 +0900

    Add reset function

--- app/src/main/java/com/example/memvoca/HomeFragment.java
+++ app/src/main/java/com/example/memvoca/HomeFragment.java
@@ -67,13 +67,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        TextView name_tv = (TextView)v.findViewById(R.id.name_tv);
         LinearLayout box_layout = (LinearLayout)v.findViewById(R.id.box_layout);
         LinearLayout test_layout = (LinearLayout)v.findViewById(R.id.test_layout);
         LinearLayout setting_layout = (LinearLayout)v.findViewById(R.id.setting_layout);
-
-        String name = PreferenceManager.getString(getContext(), "user_name") + "님";
-        name_tv.setText(name);
 
         box_layout.setOnClickListener(this);
         test_layout.setOnClickListener(this);
@@ -84,8 +80,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
 
     public void onClick(View view) {
         Intent intent = new Intent(getActivity(), FunctionActivity.class);
+
         switch (view.getId()){
-            //출근버튼
             case R.id.box_layout:
                 intent.putExtra("title","학습 박스");
                 intent.putExtra("type","box");
@@ -98,7 +94,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 break;
             case R.id.setting_layout:
                 String name = PreferenceManager.getString(getContext(), "user_name") + "님";
-                intent.putExtra("title",name);
+                intent.putExtra("title","설정");
                 intent.putExtra("type","setting");
                 startActivity(intent);
                 break;
--- app/src/main/java/com/example/memvoca/SettingFragment.java
+++ app/src/main/java/com/example/memvoca/SettingFragment.java
@@ -1,21 +1,87 @@
 package com.example.memvoca;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment implements View.OnClickListener {
 
+    private static final int RESULT_OK = -1;
     private View view;
+    private Context mContext;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_setting, container, false);
+        mContext = getContext();
+
+        TextView mem_info = (TextView)view.findViewById(R.id.edit_mem_info);
+        TextView reset_target = (TextView)view.findViewById(R.id.reset_target);
+        TextView cycle_reset = (TextView)view.findViewById(R.id.cycle_reset);
+
+        mem_info.setOnClickListener(this);
+        reset_target.setOnClickListener(this);
+        cycle_reset.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getActivity(), ResetPopupActivity.class);
+
+        switch (view.getId()){
+            case R.id.edit_mem_info:
+                intent.putExtra("title","닉네임을");
+                intent.putExtra("type","reset_name");
+                startActivityForResult(intent, 1);
+                break;
+            case R.id.reset_target:
+                intent.putExtra("title","목표치를");
+                intent.putExtra("type","reset_word");
+                startActivityForResult(intent, 1);
+                break;
+            case R.id.cycle_reset:
+                intent.putExtra("title","주기를");
+                intent.putExtra("type","reset_day");
+                startActivityForResult(intent, 1);
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1){
+            if(resultCode==RESULT_OK){
+                String result = data.getStringExtra("data");
+                String type = data.getStringExtra("type");
+
+                switch (type) {
+                    case "reset_name":
+                        PreferenceManager.setString(mContext, "user_name", result+"님");
+                        break;
+                    case "reset_word":
+                        PreferenceManager.setInt(mContext, "word_target_setting", Integer.parseInt(result));
+                        break;
+                    case "reset_day":
+//                        PreferenceManager.setInt(mContext, "day1_target_setting", Integer.parseInt(result1));
+//                        PreferenceManager.setInt(mContext, "day2_target_setting", Integer.parseInt(result2));
+//                        PreferenceManager.setInt(mContext, "day3_target_setting", Integer.parseInt(result3));
+//                        PreferenceManager.setInt(mContext, "day4_target_setting", Integer.parseInt(result4));
+//                        PreferenceManager.setInt(mContext, "day5_target_setting", Integer.parseInt(result5));
+                        break;
+                }
+            }
+        }
     }
 }
